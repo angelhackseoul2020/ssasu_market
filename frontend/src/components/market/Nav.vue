4163f93ca9a03b63fb2ca75b1407eae80d6cbe13
SeolYu, Lee <seolyu.90@gmail.com>
2020-07-16 19:03:26 +0900

    [feat] Make nav icon image

--- frontend/src/components/market/Nav.vue
+++ frontend/src/components/market/Nav.vue
@@ -1,41 +1,77 @@
 <template>
   <div class="nav">
-      <router-link style="text-decoration: none; color: inherit;" to="/market/home"><div class="home">홈</div></router-link>
-      <router-link style="text-decoration: none; color: inherit;" to="/market/store"><div class="store">상점</div></router-link>
-      <router-link style="text-decoration: none; color: inherit;" to="/market/community"><div class="community">게시판</div></router-link>
+    <router-link style="text-decoration: none; color: inherit;" to="/market/home">
+      <div class="home">
+        <img class="homeIcon" src="../../assets/imgs/marketHome.png" alt />홈
+      </div>
+    </router-link>
+    <router-link style="text-decoration: none; color: inherit;" to="/market/store">
+      <div class="store">
+        <img class="storeIcon" src="../../assets/imgs/marketStore.png" alt />상점
+      </div>
+    </router-link>
+    <router-link style="text-decoration: none; color: inherit;" to="/market/community">
+      <div class="community">
+        <img class="communityIcon" src="../../assets/imgs/marketCommunity.png" alt />게시판
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-    name: "Nav"
-}
+  name: "Nav"
+};
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Jua&display=swap");
 
 .nav {
-    background-color: #fdafd063;
-    border-right: 1px solid rgba(143, 141, 141, 0.534);
-    font-family: "Jua", sans-serif;
-    font-size: 3rem;
-    color: #2E0202;
-    text-align: center;
+  background-color: #fdafd063;
+  border-right: 1px solid rgba(143, 141, 141, 0.534);
+  font-family: "Jua", sans-serif;
+  color: #2e0202;
+  text-align: center;
 }
 
 .home {
-    height: 33.3vh;
-    line-height: 33.3vh;
-    border-bottom: 1px solid rgba(143, 141, 141, 0.534);
+  height: 33.3vh;
+  /* line-height: 33.3vh; */
+  border-bottom: 1px solid rgba(143, 141, 141, 0.534);
+  font-size: 1.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.homeIcon {
+  width: 140px;
 }
 .store {
-    height: 33.3vh;
-    line-height: 33.3vh;
-    border-bottom: 1px solid rgba(143, 141, 141, 0.534);
+  height: 33.3vh;
+  /* line-height: 33.3vh; */
+  border-bottom: 1px solid rgba(143, 141, 141, 0.534);
+  font-size: 1.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
-.community{
-    height: 33.3vh;
-    line-height: 33.3vh;
+.storeIcon {
+  width: 140px;
+}
+.community {
+  height: 33.3vh;
+  /* line-height: 33.3vh; */
+  border-bottom: 1px solid rgba(143, 141, 141, 0.534);
+  font-size: 1.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.communityIcon {
+  width: 130px;
 }
 </style>
