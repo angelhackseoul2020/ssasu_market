aecfef883a09f8f3b40fca9f76208aafb0d44a0e
SeolYu, Lee <seolyu.90@gmail.com>
2020-07-16 13:33:07 +0900

    [feat] Make admin add page

--- frontend/src/components/admin/AdminStoreAdd.vue
+++ frontend/src/components/admin/AdminStoreAdd.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="adminStoreAddWrap">
-    <div class="adminStoreAdd">상점 추가</div>
+    <div class="adminStoreAdd">
+      <form action method="GET">
+        <label for="storeName">상점 이름</label>
+        <input type="text" id="storeName" placeholder="상점 이름" required />
+
+        <label for="storeAddress">주소</label>
+        <input type="text" id="storeAddress" placeholder="주소" />
+
+        <label for="storeTell">연락처</label>
+        <input type="text" id="storeTell" placeholder="연락처" />
+
+        <label for="storeMemo">메모</label>
+        <input type="text" id="storeMemo" placeholder="메모" />
+
+        <div class="deleteSave">
+          <button type="submit" id="delete">delete</button>
+          <button type="submit" id="save">save</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -21,5 +40,46 @@ export default {
   margin: 3rem 3rem;
   background-color: rgba(252, 252, 252, 0.589);
   border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.adminStoreAdd {
+}
+label {
+  display: block;
+  width: 100%;
+  font-size: 12px;
+  line-height: 16px;
+}
+input {
+  width: 300px;
+  height: 50px;
+  font-size: 2rem;
+  background-color: #fdafd063;
+  border-radius: 20px;
+  border: 1.2px solid rgba(245, 245, 245, 0.575);
+}
+input:focus,
+input:hover,
+input:active {
+  outline: none;
+  box-shadow: none;
+  border-color: #f3c0d8;
+}
+button {
+  width: 100px;
+  height: 40px;
+  font-size: 1.3rem;
+  text-align: center;
+  border-style: none;
+  border-radius: 40px;
+  cursor: pointer;
+}
+#delete {
+  background-color: rgb(156, 4, 4);
+}
+#save {
+  background-color: #26833d;
 }
 </style>
--- frontend/src/components/admin/AdminUserAdd.vue
+++ frontend/src/components/admin/AdminUserAdd.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="adminUserAddWrap">
-    <div class="adminUserAdd">유저 추가</div>
+    <div class="adminUserAdd">
+      <form action method="GET">
+        <label for="userId">아이디</label>
+        <input type="text" id="userId" placeholder="아이디" required />
+
+        <label for="userNickname">닉네임</label>
+        <input type="text" id="userNickname" placeholder="닉네임" />
+
+        <label for="userEmail">이메일</label>
+        <input type="text" id="userEmail" placeholder="이메일" />
+
+        <label for="userTell">연락처</label>
+        <input type="text" id="userTell" placeholder="연락처" />
+
+        <div class="deleteSave">
+          <button type="submit" id="delete">delete</button>
+          <button type="submit" id="save">save</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -21,5 +40,46 @@ export default {
   margin: 3rem 3rem;
   background-color: rgba(252, 252, 252, 0.589);
   border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.adminUserAdd {
+}
+label {
+  display: block;
+  width: 100%;
+  font-size: 12px;
+  line-height: 16px;
+}
+input {
+  width: 300px;
+  height: 50px;
+  font-size: 2rem;
+  background-color: #fdafd063;
+  border-radius: 20px;
+  border: 1.2px solid rgba(245, 245, 245, 0.575);
+}
+input:focus,
+input:hover,
+input:active {
+  outline: none;
+  box-shadow: none;
+  border-color: #f3c0d8;
+}
+button {
+  width: 100px;
+  height: 40px;
+  font-size: 1.3rem;
+  text-align: center;
+  border-style: none;
+  border-radius: 40px;
+  cursor: pointer;
+}
+#delete {
+  background-color: rgb(156, 4, 4);
+}
+#save {
+  background-color: #26833d;
 }
 </style>
