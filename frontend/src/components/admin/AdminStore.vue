5015a420bce28471ff357fb59628211ff8a8d119
SeolYu, Lee <seolyu.90@gmail.com>
2020-07-16 14:53:52 +0900

    [feat] make add page

--- frontend/src/components/admin/AdminStore.vue
+++ frontend/src/components/admin/AdminStore.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="adminStoreWrap">
-    <div class="adminStore">상점리스트</div>
+    <div class="adminStore">
+      <div class="header">
+        <button type="submit" class="ddStore">상점 추가</button>
+
+        <button type="submit" class="addStore">상점 추가</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,5 +27,22 @@ export default {
   margin: 3rem 3rem;
   background-color: rgba(252, 252, 252, 0.589);
   border-radius: 15px;
+}
+.header {
+  display: flex;
+  background-color: pink;
+  justify-content: flex-end;
+}
+.addStore {
+  background-color: #fdafd063;
+  outline: 0;
+  width: 130px;
+  font-size: 1.6rem;
+  color: #2e0202;
+  border-style: none;
+  border-radius: 40px;
+}
+.addStore:hover {
+  cursor: pointer;
 }
 </style>
