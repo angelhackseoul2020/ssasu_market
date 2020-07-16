d33c4405a5acd288d47059ee255aec6a28f63dc2
paik11012 <minju11012@gmail.com>
2020-07-16 18:31:26 +0900

    [fix] Settings about cors policy

--- backend/angelhack/settings.py
+++ backend/angelhack/settings.py
@@ -51,8 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL=True
 ROOT_URLCONF = 'angelhack.urls'
 
 TEMPLATES = [
