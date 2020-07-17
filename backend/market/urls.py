2fd0cadb9ec32f969eb2725a8c94562f81364c07
GaYoung87 <gyyoon4u@naver.com>
2020-07-17 20:37:38 +0900

    [feat] Add visitor info

--- backend/market/urls.py
+++ backend/market/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('info/', views.info, name='info'),
     path('makevisitor/', views.makevisitor, name='makevisitor'),
+    path('qrcode_page/', views.qrcode_page, name='qrcode_page'),
+    path('qr_code/', include('qr_code.urls', namespace="qr_code")),
 ]
--- backend/market/views.py
+++ backend/market/views.py
@@ -6,7 +6,9 @@ from datetime import datetime
 from .serializers import MarketSerializer, VisitorSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny 
+from rest_framework.permissions import AllowAny
+from qr_code.qrcode.utils import ContactDetail, QRCodeOptions
+
 
 # market 관련 api
 @api_view(['GET'])
@@ -16,6 +18,7 @@ def info(request):
     serializer = MarketSerializer(markets, many=True)
     return Response(serializer.data)
 
+
 # visitor 정보 저장 api
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -24,3 +27,22 @@ def makevisitor(request):
     if serializer.is_valid(raise_exception=True):
         serializer.save()
         return HttpResponse('Sucess Visitor')
+
+
+# qrcode 보여주는 api
+DEMO_CONTACT = ContactDetail(
+        name='gyyoon4u',
+        market='tong-in market',
+    )
+DEMO_OPTIONS = QRCodeOptions(size='t', border=6, error_correction='L')
+
+def qrcode_page(request):
+    visitor = Visitor.objects.only()
+    context = dict(
+        contact_detail=DEMO_CONTACT,
+        options_example=DEMO_OPTIONS,
+        visitor=visitor['user_id']
+    )
+
+    # Render the index page.
+    return render(request, 'market/qrcode_page.html', context=context)
