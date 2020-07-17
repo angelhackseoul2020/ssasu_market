3f57c815d722b270601ed8b7ca03761aa6e9cb75
DESKTOP-SMQNLVA\PARK <nan308@naver.com>
2020-07-17 23:29:37 +0900

    [feat/userListPage] Make userListPage

--- frontend/src/components/admin/AdminUser.vue
+++ frontend/src/components/admin/AdminUser.vue
@@ -1,13 +1,66 @@
 <template>
   <div class="adminUserWrap">
     <div class="adminUser">유저리스트</div>
+    <button id="userAddBtn">유저 추가</button>
+    <div id="colId">
+      <p id="colHead">ID</p>
+      <p id="userData" v-for="user in userList" :key="user">{{user.name}}</p>
+    </div>
+    <div id="colNick">
+      <p id="colHead">닉네임</p>
+      <p id="userData" v-for="user in userList" :key="user">{{user.nick}}</p>
+    </div>
+    <div id="colAddress">
+      <p id="colHead">주소</p>
+      <p id="userData" v-for="user in userList" :key="user">{{user.address}}</p>
+    </div>
+    <div id="colReview">
+      <p id="colHead">리뷰</p>
+      <p id="userData" v-for="user in userList" :key="user">{{user.review}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "AdminUser",
-  components: {}
+  components: {},
+  data(){
+    return{
+      userList:[
+        {
+          'name':'이설유',
+          'nick':'soulyu',
+          'address':'부천',
+          'review':4
+        },
+        {
+          'name':'김윤재',
+          'nick':'yoonjae',
+          'address':'봉천',
+          'review':3        
+        },
+        {
+          'name':'박권응',
+          'nick':'kwoneyng',
+          'address':'신림',
+          'review':4        
+        },
+        {
+          'name':'백민주',
+          'nick':'minzu',
+          'address':'일산',
+          'review':1        
+        },
+        {
+          'name':'윤가영',
+          'nick':'gayoung',
+          'address':'서울대입구',
+          'review':2        
+        },
+      ]
+    }
+  }
 };
 </script>
 
@@ -18,8 +71,58 @@ export default {
   box-sizing: border-box;
 }
 .adminUserWrap {
+  position: relative;
   margin: 3rem 3rem;
   background-color: rgba(252, 252, 252, 0.589);
   border-radius: 15px;
+  overflow: auto;
+}
+#colId{
+  position: absolute;
+  top: 15%;
+  width: 200px;
+  left: 15%;
+  font-size: 40px;
+}
+#colNick{
+  position:absolute;
+  width: 200px;
+  top: 15%;
+  left: 35%;
+  font-size: 40px;
+}
+#colAddress{
+  position: absolute;
+  width: 200px;
+  top: 15%;
+  left: 55%;
+  font-size: 40px;
+}
+#colReview{
+  position: absolute;
+  width: 200px;
+  top: 15%;
+  left: 75%;
+  font-size: 40px;
+}
+#colHead{
+  font-size: 50px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+#userData{
+  margin-bottom: 15px;
+  text-align: center;
+}
+#userAddBtn{
+  position:absolute;
+  width: 180px;
+  height: 60px;
+  font-size: 30px;
+  background-color: rgb(255,200,200);
+  border-radius: 30px;
+  border: solid 1px white;
+  right:5%;
+  top:5%
 }
 </style>
