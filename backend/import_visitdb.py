22bd1b1da48d4d3b95f9443333fb6a567b945a64
paik11012 <minju11012@gmail.com>
2020-07-17 18:26:49 +0900

    [fix] Put visitors csv files into sqlite3

---
+++ backend/import_visitdb.py
@@ -0,0 +1,32 @@
+import csv
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "angelhack.settings")
+django.setup()
+
+from market.models import VisitDatabase, Market
+
+# 첫 번째 파일
+CSV_PATH = ['./datainput/S-DoT_WALK_2020.05.04(34120).csv','./datainput/S-DoT_WALK_2020.05.11(23584).csv','./datainput/S-DoT_WALK_2020.05.18(19041).csv',
+'./datainput/S-DoT_WALK_2020.05.25(41364).csv','./datainput/S-DoT_WALK_2020.06.01(41038).csv',
+'./datainput/S-DoT_WALK_2020.06.15(40807).csv','./datainput/S-DoT_WALK_2020.06.22(40666).csv',
+'./datainput/S-DoT_WALK_2020.06.29(35765).csv']
+for i in range(len(CSV_PATH2)): # 8개의 파일 동안 반복되도록
+    with open(CSV_PATH2[i], newline='') as csvfile:
+        data_reader = csv.DictReader(csvfile)
+        for row in data_reader:
+            market_name = row['시장']
+            try:
+                market = Market.objects.get(name=market_name)
+                # market.id를 매개로 해서 데이터 넣기
+                VisitDatabase.objects.create(                
+                    name_id = market.id,
+                    date = row['등록일자'],
+                    number = row['방문자수']
+                )
+            except:
+                continue     
+
+        print("done", i)
+            
